6a0fdeae9f047af0207e8d65fa96c638fecc56d3
Harouna <hrncsk@yahoo.fr>
2023-03-02 13:51:15 +0100

    set responseType by cud

--- product_frontend/src/app/services/product/product.service.ts
+++ product_frontend/src/app/services/product/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from 'src/app/model/product';
@@ -12,19 +12,20 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   public findAll(): Observable<Product[]> {
+
     return this.http.get<Product[]>(`${this.producturl}all`);
   }
 
 
   public save(product: Product): Observable<string> {
-    return this.http.post<string>(`${this.producturl}add`, product);
+    return this.http.post(`${this.producturl}add`, product, { responseType: 'text' });
   }
 
   public delete(id: number): Observable<string> {
-    return this.http.delete<string>(`${this.producturl}delete/${id}`);
+    return this.http.delete(`${this.producturl}delete/${id}`, { responseType: 'text' });
   }
 
   public update(product: Product): Observable<string> {
-    return this.http.put<string>(`${this.producturl}update`, product);
+    return this.http.put(`${this.producturl}update`, product, { responseType: 'text' });
   }
 }
